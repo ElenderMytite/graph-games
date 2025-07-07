d0ea32a28d7aee5cd9fbdd8ce15537ef843b326e
ElenderMytite <ElenderMytite>
2025-07-07 14:15:38 +0200

    Fixed crash bug on move command; added remove node and pin commands.

--- src/cmd_editor.rs
+++ src/cmd_editor.rs
@@ -7,33 +7,45 @@ use crate::{graph::Graph, visitor::Visitor};
 pub struct CommandLineEditor{
     graph: Rc<RefCell<Graph>>,
     visitors: HashMap<String, Visitor>,
-    current_visitor: String,
+    current_visitor: Option<String>,
+    placeholder: usize,
 }
 impl CommandLineEditor {
     pub fn new() -> Self {
         let graph = Rc::new(RefCell::new(Graph::new()));
-        graph.borrow_mut().add_node("#default".to_string());
         Self {
-            visitors: HashMap::from([("#default".to_string(), Visitor::new(Rc::clone(&graph), "#default".to_string()))]),
-            current_visitor: String::from("#default".to_string()),
+            visitors: HashMap::new(),
+            current_visitor: None,
             graph,
+            placeholder: 0,
         }
     }
     
     pub fn pin(&mut self, name: String, visitor: Visitor) {
         self.visitors.insert(name, visitor);
     }
+    pub fn next_free(&mut self) -> String
+    {
+        self.placeholder += 1;
+        format!("{}", self.placeholder)
+    }
     
     pub fn go_to_pin(&mut self, name: String) {
         if self.visitors.contains_key(&name) {
-            self.current_visitor = name;
+                self.current_visitor = Some(name);
         } else {
-            println!("Visitor {} not found.", name);
+                println!("Visitor {} not found.", name);
         }
     }
-    
-    pub fn current_pin(&self) -> Visitor {
-        self.visitors.get(&self.current_visitor).unwrap().clone()
+
+    pub fn current_pin(&self) -> Result<Visitor,&str> {
+        match self.current_visitor {
+            Some(ref name) => {
+                Ok(self.visitors[name].clone())
+            },
+            None => Err("No current visitor set."),
+            
+        }
     }
     pub fn run(&mut self) {
         loop {
@@ -41,9 +53,10 @@ impl CommandLineEditor {
             io::stdout().flush().unwrap();
             let mut command = String::new();
             io::stdin().read_line(&mut command).expect("Failed to read line");
-            let mut command: Vec<&str> = command.trim().split(" ").collect();
+            let mut command: Vec<String> = command.trim().split(" ").map(|s| s.to_string()).collect();
             command.reverse();
-            if self.execute_command(self.parse_command(&mut command))
+            let exe_command = self.parse_command(&mut command);
+            if self.execute_command(exe_command)
             {
                 break;
             }
@@ -54,49 +67,77 @@ impl CommandLineEditor {
         for command in commands{
         match command {
             Command::Exit => return true,
-            Command::Node(name) => self.graph.borrow_mut().add_node(name),
-            Command::Edge(n1, n2, edge_name) => self.graph.borrow_mut().connect(n1, n2, edge_name),
-            Command::Pin(visitor_name, visitor) => self.pin(visitor_name, visitor),
+            Command::CreateNode(name) => self.graph.borrow_mut().add_node(name),
+            Command::CreateEdge(n1, n2, edge_name) => self.graph.borrow_mut().connect(n1, n2, edge_name),
+            Command::CreatePin(visitor_name, visitor) => self.pin(visitor_name, visitor),
+            Command::RemoveNode(name) => {
+                if self.graph.borrow_mut().nodes.remove(&name).is_some() {
+                    println!("Node {} removed.", name);
+                } else {
+                    println!("Node {} not found.", name);
+                }
+            },
+            Command::RemovePin(name) => {
+                if self.visitors.remove(&name).is_some() {
+                    println!("Visitor {} removed.", name);
+                } else {
+                    println!("Visitor {} not found.", name);
+                }
+            },
             Command::Switch(visitor_name) => self.go_to_pin(visitor_name),
             Command::Current => {
-                let visitor = self.current_pin();
-                visitor.print_current();
+                match self.current_pin() {
+                    Ok(visitor) => visitor.print_current(),
+                    Err(e) => println!("Error from current pin: {}", e),
+                }
             },
             Command::ListNeighbours => {
-                let visitor = self.current_pin();
-                visitor.show_neighbours();
+                match self.current_pin()
+                {
+                        Ok(visitor) => visitor.show_neighbours(),
+                        Err(e) => println!("Error from list neighbours: {}", e),
+                }
             },
             Command::None => (),
             Command::Move(visitor_name, edge) => {
-                let mut visitor = self.current_pin();
-                visitor.move_to(edge);
-                self.pin(visitor_name, visitor);
+                if let Some(visitor) = self.visitors.get_mut(&visitor_name) {
+                    visitor.move_to(edge);
+                }
+
             },
         }}
         false
     }
-    fn parse_command(&self, command: &mut Vec<&str>) -> Vec<Command> {
+    fn parse_command(&mut self, command: &mut Vec<String>) -> Vec<Command> {
         let mut commands = vec![];
-        commands.push(match command.pop().unwrap_or("").trim() {
+        commands.push(match command.pop().unwrap_or(String::from("")).trim() {
             "ex" | "exit" => Command::Exit,
             "nd" | "node" => {
                 let name = self.parse_node_name(command);
-                Command::Node(name.trim().to_string())
+                Command::CreateNode(name.trim().to_string())
+            },
+            "nr" | "noderemove" => {
+                let name = command.pop().unwrap_or_default();
+                Command::RemoveNode(name.trim().to_string())
             },
             "eg" | "edge" => {
                 let n1 = self.parse_node_name(command);
                 let n2 = self.parse_node_name(command);
-                let edge_name = command.pop().unwrap_or("placeholder").trim();
-                Command::Edge(n1.trim().to_string(), n2.trim().to_string(), edge_name.trim().to_string())
+                let edge_name: String = command.pop().unwrap_or_else(|| -> String {String::from("edge") + self.next_free().as_str()}).trim().to_string();
+                Command::CreateEdge(n1.trim().to_string(), n2.trim().to_string(), edge_name.trim().to_string())
             },
             "pn" | "pin" => {
-                let visitor_name = command.pop().unwrap_or("visitor").trim();
+                let visitor_name = command.pop().unwrap_or_else(||-> String {String::from("visitor") + self.next_free().as_str()}).trim().to_string();
                 let node_name = self.parse_node_name(command); 
-                let visitor = Visitor::new(self.current_pin().place, node_name.trim().to_string());
-                Command::Pin(visitor_name.trim().to_string(), visitor)
+                let visitor = Visitor::new(Rc::clone(&self.graph), Some(node_name.trim().to_string()));
+                Command::CreatePin(visitor_name.to_string(), visitor)
+            },
+            "pr" | "pinremove" => {
+                let name = command.pop().unwrap_or_default();
+                Command::RemovePin(name.trim().to_string())
             },
             "sw" | "swich"  => {
-                let visitor_name = command.pop().unwrap_or("visitor").trim();
+                let visitor_name = command.pop().unwrap_or_else(||-> String {String::from("visitor") + self.next_free().as_str()}).trim().to_string();
                 Command::Switch(visitor_name.trim().to_string())
             },
             "cr" | "current" => {
@@ -106,23 +147,26 @@ impl CommandLineEditor {
                 Command::ListNeighbours
             },
             "la" | "listall" =>{
-                self.graph.borrow().nodes.iter().for_each(|(name, node)| {
+                self.graph.borrow().nodes.iter().for_each(|(name,_)| {
                     println!("Node: {}", name);
-                    for (edge_name, neighbour) in node.borrow().edges.iter() {
-                        println!("  Edge: {}, Neighbour: {}", edge_name, neighbour.borrow().name);
-                    }
                 });
                 Command::None
             }
             "lv" | "listvisitors" => {
                 self.visitors.iter().for_each(|(name, visitor)| {
-                    println!("Visitor: {}, Current Node: {}", name, visitor.current);
+                    println!("Visitor: {}, Current Node: {}", name, visitor.current.clone().unwrap_or("None".to_string()));
                 });
                 Command::None
             },
             "mv" | "move" => {
-                let edge = command.pop().unwrap_or("way").trim();
-                let visitor_name = command.pop().unwrap_or("visitor").trim();
+                let visitor_name = command.pop()
+                .unwrap_or_else(||-> String {String::from("visitor") + self.next_free().as_str()})
+                .trim()
+                .to_string();
+                let edge = command.pop()
+                .unwrap_or_else(||-> String {String::from("edge") + self.next_free().as_str()})
+                .trim().
+                to_string();
                 Command::Move(visitor_name.trim().to_string(), edge.trim().to_string())
             },
             "" => Command::None,
@@ -130,14 +174,14 @@ impl CommandLineEditor {
         });
         commands
     }
-    fn parse_node_name(&self, command: &mut Vec<&str>) -> String {
+    fn parse_node_name(&self, command: &mut Vec<String>) -> String {
         if let Some(name) = command.pop() {
             if name.starts_with("@") {
                 return self.visitors.get(&name[1..])
                     .map_or_else(|| {
                         println!("Visitor {} not found.", name);
                         String::new()
-                    }, |v| v.current.clone());
+                    }, |v| v.current.clone().unwrap());
             }
             name.to_string()
         } else {
@@ -147,9 +191,11 @@ impl CommandLineEditor {
 }
 enum Command {
     Exit,
-    Node(String),
-    Edge(String, String, String),
-    Pin(String, Visitor),
+    CreateNode(String),
+    CreateEdge(String, String, String),
+    CreatePin(String, Visitor),
+    RemoveNode(String),
+    RemovePin(String),
     Switch(String),
     Move(String, String),
     Current,
--- src/visitor.rs
+++ src/visitor.rs
@@ -5,29 +5,40 @@ use crate::graph::Graph;
 #[derive(Clone)]
 pub struct Visitor {
     pub place: Rc<RefCell<Graph>>,
-    pub current: String,
+    pub current: Option<String>,
 }
 impl Visitor{
-    pub fn new(place: Rc<RefCell<Graph>>, current: String) -> Self {
+    pub fn new(place: Rc<RefCell<Graph>>, current: Option<String>) -> Self {
         Self{ place: place, current }
     }
     pub fn print_current(&self) {
-        println!("Current node: {}", self.current);
+        println!("Current node: {}", self.current.clone().unwrap_or("None".to_string()));
     }
     pub fn show_neighbours(&self) {
-        if let Some(node) = self.place.borrow().nodes.get(&self.current) {
+
+        if let Some(current) = self.current.clone() {        
+            if let Some(node) = self.place.borrow().nodes.get(&current) {
             for (edge_name, neighbour) in node.borrow().edges.iter() {
                 println!("Edge: {}, Neighbour: {}", edge_name, neighbour.borrow().name);
             }
-        } else {
-            println!("Node this visitor's node {} not found in the graph.", self.current);
+            } else{
+            println!("Node this visitor's node {} not found in the graph.", current);
+            }
         }
     }
     pub fn move_to(&mut self, path: String) {
-        if self.place.borrow().nodes.contains_key(&self.current) {
-            self.current = self.place.borrow().nodes[&self.current].borrow().edges.get(&path).unwrap().borrow().name.clone();
-        } else {
-            println!("Path {} doesn't exist for current node: {}.", path, self.current);
+        if let Some(current) = self.current.clone() {
+            if self.place.borrow().nodes.contains_key(&current) {
+                self.current = Some(self.place.borrow().nodes[&current].borrow()
+                .edges.get(&path)
+                .unwrap_or(&(self.place.borrow().nodes[&self.current.clone().unwrap()])).borrow()
+                .name.clone());
+            } else {
+                println!("Path {} doesn't exist for current node: {}.", path, current);
+            }
+        }
+        else {
+            println!("No current node set for this visitor.");
         }
     }
 }
